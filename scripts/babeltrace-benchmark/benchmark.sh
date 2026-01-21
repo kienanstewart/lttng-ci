bb5b12d5db01b5df4560dc30262cb8c38e3dfbe3
Kienan Stewart <kstewart@efficios.com>
2026-01-21 15:26:37 -0500

    jjb: babeltrace: Publish benchmarks as HTML report
    Change-Id: I4706cf30ed82277db3784c132aa12fcf2d411c96
    Signed-off-by: Kienan Stewart <kstewart@efficios.com>

--- scripts/babeltrace-benchmark/benchmark.sh
+++ scripts/babeltrace-benchmark/benchmark.sh
@@ -40,22 +40,44 @@ fi
 
 # Run the lava jobs
 exit_code=0
-python "$SCRIPT_PATH" \
-    --generate-jobs \
-    --bt-repo-path "$SRC_DIR" \
-    --batch-size "${BENCHMARK_BATCH_SIZE}" \
-    $FORCE_ARG $TAGS_ARG \
-    --max-batches "${BENCHMARK_MAX_BATCHES}" \
-    --ci-repo "${LTTNG_CI_REPO}" \
-    --ci-branch "${LTTNG_CI_BRANCH}" \
-    --nfs-root-url "${NFS_ROOT_URL}" \
-    --kernel-url "${S3_HTTP_BUCKET_URL}/system-tests/kernel/${KERNEL_COMMIT_ID}.baremetal.bzImage" || exit_code=1
+if [[ "${BENCHMARK_SKIP_SUBMISSION:-}" != "true" ]]; then
+    python "$SCRIPT_PATH" \
+           --generate-jobs \
+           --bt-repo-path "$SRC_DIR" \
+           --batch-size "${BENCHMARK_BATCH_SIZE}" \
+           $FORCE_ARG $TAGS_ARG \
+           --max-batches "${BENCHMARK_MAX_BATCHES}" \
+           --ci-repo "${LTTNG_CI_REPO}" \
+           --ci-branch "${LTTNG_CI_BRANCH}" \
+           --nfs-root-url "${NFS_ROOT_URL}" \
+           --kernel-url "${S3_HTTP_BUCKET_URL}/system-tests/kernel/${KERNEL_COMMIT_ID}.baremetal.bzImage" || exit_code=1
+fi
 
-# Generate the report pdf
-python "$SCRIPT_PATH" \
-    --generate-report \
-    --bt-repo-path "$SRC_DIR" \
-    --report-name "${RESULTS_DIR}/babeltrace-benchmark.pdf"
+if [[ "${BENCHMARK_SKIP_PDF_GENERATION:-}" != "true" ]]; then
+    # Generate the report pdf
+    python "$SCRIPT_PATH" \
+           --generate-report \
+           --bt-repo-path "$SRC_DIR" \
+           --report-name "${RESULTS_DIR}/babeltrace-benchmark.pdf"
+fi
+
+if [[ "${BENCHMARK_SKIP_ASV_GENERATION:-}" != "true" ]]; then
+    # If this isn't done, the repo is in a detached head state that
+    # asv doesn't like (e.g. `git rev-list master --first-parent master --` fails).
+    pushd "${SRC_DIR}"
+    git checkout master
+    # Need the other branches too
+    git branch stable-2.1 origin/stable-2.1
+    git branch stable-2.0 origin/stable-2.0
+    git branch stable-1.5 origin/stable-1.5
+    popd
+
+    python "${SCRIPT_PATH}" \
+           --generate-asv-report \
+           --bt-repo-path "${SRC_DIR}" \
+           ${TAGS_ARG}
+    cp -r asv "${RESULTS_DIR}/"
+fi
 
 rm -rf "$VENV"
 exit $exit_code
