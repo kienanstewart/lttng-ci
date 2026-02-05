d583d5f70c4dcb10d0ec422c37e07009f3fbffb4
Kienan Stewart <kstewart@efficios.com>
2026-02-05 15:56:26 -0500

    jjb: Include stable-2.13 in lttng-ust-benchmarks
    Change-Id: I83de780db1d698f9254c5d5ab833ea725e23a349
    Signed-off-by: Kienan Stewart <kstewart@efficios.com>

--- scripts/lttng-ust-benchmarks/benchmark.py
+++ scripts/lttng-ust-benchmarks/benchmark.py
@@ -452,7 +452,7 @@ def _get_parser():
             "master": "871e256dcc4c2799a5386f7d9f6e9e78307ee688",
             "stable-2.15": "871e256dcc4c2799a5386f7d9f6e9e78307ee688",
             "stable-2.14": "d40d5e5e6af9fce8608ef3ba85a92f601a26b867",
-            # "stable-2.13": "06f280fd4452f88ce67e622c4961e11ad376f469",
+            "stable-2.13": "06f280fd4452f88ce67e622c4961e11ad376f469",
         },
         help="A comma-separated list of branches in the format NAME[:CUTOFF] to check",
     )
