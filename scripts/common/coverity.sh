kienanstewart/lttng-ci

commit 60c207c1de072a9432b7269f7a02b39ed002cbd7

Fix shellcheck warnings
Change-Id: Id7a4edf1389426ae300b9663fb634b415c194fd9
Signed-off-by: Kienan Stewart <kstewart@efficios.com>

scripts/common/coverity.sh
@@ -16,7 +16,6 @@ WORKSPACE=${WORKSPACE:-}
 COVERITY_SCAN_DESCRIPTION="Automated CI build"
 COVERITY_SCAN_NOTIFICATION_EMAIL="ci-notification@lists.lttng.org"
 COVERITY_SCAN_BUILD_OPTIONS=""
-#COVERITY_SCAN_BUILD_OPTIONS=("--return-emit-failures 8" "--parse-error-threshold 85")
 
 DEPS_INC="$WORKSPACE/deps/build/include"
 DEPS_LIB="$WORKSPACE/deps/build/lib"
@@ -158,7 +157,7 @@ autotools)
       ./configure "${CONF_OPTS[@]}"
     fi
 
-    cov-build --dir "$RESULTS_DIR" ${COVERITY_SCAN_BUILD_OPTIONS[@]} make -j"$NPROC" V=1
+    cov-build --dir "$RESULTS_DIR" "${COVERITY_SCAN_BUILD_OPTIONS[@]}" make -j"$NPROC" V=1
     ;;
 *)
     echo "Unsupported build type: $BUILD_TYPE"

automation/ansible/roles/cross-compilers/files/script.sh
@@ -9,8 +9,11 @@ PATCH_DIR="${PATCH_DIR:-/src/patches}"
 BIN_SUFFIX="${BIN_SUFFIX:-4.8}"
 IFS=' ' read -r -a TARGETS <<< "${TARGETS:-aarch64-linux-gnu}"
 HOST="${HOST:-x86_64-pc-linux-gnu}"
+# shellcheck disable=SC2206
 CONFIGURE_ARGS=(${CONFIGURE_ARGS:-})
+# shellcheck disable=SC2206
 MAKE_ARGS=(${MAKE_ARGS:-})
+# shellcheck disable=SC2206
 MAKE_INSTALL_ARGS=(${MAKE_INSTALL_ARGS:-})
 DEBUG="${DEBUG:-}"
 CSTD="${CSTD:-gnu99}"

automation/create-slave.sh
@@ -27,12 +27,12 @@ ci-slave-x64-02-01 amd64 52:54:00:c3:1a:30 cloud02.internal.efficios.com
 ci-slave-x64-02-02 amd64 52:54:00:b1:92:a3 cloud02.internal.efficios.com
 ci-slave-x64-02-03 amd64 52:54:00:3a:6b:ca cloud02.internal.efficios.com
 ci-slave-x64-02-04 amd64 52:54:00:c9:91:d1 cloud02.internal.efficios.com" | \
-while read node arch mac host
+while read -r node arch mac host
 do
 
-if [ "x$host" != "x" ]; then
+if [ "$host" != "" ]; then
 
-virt-install --name ${node} \
+virt-install --name "${node}" \
     --ram 4096 \
     --vcpus 8 \
     --disk pool=default,size=20 \
@@ -42,11 +42,10 @@ virt-install --name ${node} \
     --location "http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-${arch}/" \
     --initrd-inject='preseed.cfg' \
     --extra-args='debian-installer/locale=en_US.UTF-8 keyboard-configuration/layoutcode=us netcfg/choose_interface=auto hostname=unassigned' \
-    --connect=qemu+ssh://root@${host}/system \
+    --connect="qemu+ssh://root@${host}/system" \
     >/dev/null 2>&1 &
 
 sleep 10
 fi
 
 done
-

lava/benchmarks/babeltrace/benchmark.sh
@@ -3,9 +3,11 @@
 set -exu
 set -o pipefail
 
+# shellcheck source=SCRIPTDIR/../common/utils.sh disable=SC1091
 source lava/benchmarks/common/utils.sh
 
 enable_performance_cpu_governor || true
+# shellcheck disable=SC2119
 enable_coredumps || true
 
 BASE_DIR="$(pwd)"
@@ -80,7 +82,7 @@ while read -d ' ' -r commit ; do
         continue
     fi
 
-    if ! make -j$(nproc) > ../make.log 2>&1 ; then
+    if ! make -j"$(nproc)" > ../make.log 2>&1 ; then
         # Upload log
         cat ../make.log
         echo "[${commit}] make failed" >&2
@@ -104,7 +106,7 @@ while read -d ' ' -r commit ; do
 
     ldconfig
     BT_BIN=$PREFIX/bin/babeltrace2
-    if [ -a "$PREFIX/bin/babeltrace" ] ; then
+    if [ -e "$PREFIX/bin/babeltrace" ] ; then
         echo "Running bt1"
         BT_BIN=$PREFIX/bin/babeltrace
     fi

lava/benchmarks/common/utils.sh
@@ -26,16 +26,16 @@ function upload_artifact()
     set +x
     local local_file=$1
     local s3_key=$2
-    local md5
-    local CURL_RC="$(mktemp)"
-    local ERR_LOG="$(mktemp)"
+    local md5 CURL_RC ERR_LOG
+    CURL_RC="$(mktemp)"
+    ERR_LOG="$(mktemp)"
 
-    md5="$(openssl md5 -binary $local_file | openssl base64)"
+    md5="$(openssl md5 -binary "$local_file" | openssl base64)"
 
     # Fetch the S3 keys stored in secrets. Usea a subshell to avoid source into
     # the current context.
     (
-        # shellcheck disable=SC1091
+        # shellcheck disable=SC1090,SC1091
         source "/lava-${LAVA_JOB_ID}/secrets"
         echo "user = \"$S3_ACCESS_KEY:$S3_SECRET_KEY\""
     ) > "${CURL_RC}"
@@ -56,13 +56,14 @@ function delete_artifact()
 {
     set +x
     local s3_key=$1
-    local CURL_RC="$(mktemp)"
-    local ERR_LOG="$(mktemp)"
+    local CURL_RC ERR_LOG
+    CURL_RC="$(mktemp)"
+    ERR_LOG="$(mktemp)"
 
     # Fetch the S3 keys stored in secrets. Usea a subshell to avoid source into
     # the current context.
     (
-        # shellcheck disable=SC1091
+        # shellcheck disable=SC1090,SC1091
         source "/lava-${LAVA_JOB_ID}/secrets"
         echo "user = \"$S3_ACCESS_KEY:$S3_SECRET_KEY\""
     ) > "${CURL_RC}"

lava/benchmarks/lttng-ust/benchmark.sh
@@ -3,9 +3,11 @@
 set -exu
 set -o pipefail
 
+# shellcheck source=SCRIPTDIR/../common/utils.sh disable=SC1091
 source lava/benchmarks/common/utils.sh
 
 enable_performance_cpu_governor || true
+# shellcheck disable=SC2119
 enable_coredumps || true
 
 # Setup ram disk
@@ -45,7 +47,7 @@ function set_commits_from_ust_commit()
     # The first heuristic is the nearest tag. This is _not_ very good on
     # the master branch, but otherwise it's probably "ok".
     tag="$(git -C "${UST_SRC_DIR}" describe --abbrev=0 "${commit}")"
-    vmajor_minor="$(echo $tag | cut -d '.' -f 1-2)"
+    vmajor_minor="$(echo "$tag" | cut -d '.' -f 1-2)"
 
     # Use the .0 release of the major.minor
     tools_commit="${vmajor_minor}.0"
@@ -57,7 +59,8 @@ function set_commits_from_ust_commit()
 function mark_commit_failure()
 {
     local msg="${1:-Unknown failure}"
-    local file="$(mktemp)"
+    local file
+    file="$(mktemp)"
     echo "[$(date)] ${msg}" > "$file"
     upload_artifact "$file" "$RESULTS_DIR/failed"
     rm -f "$file"
@@ -87,7 +90,7 @@ function build_urcu()
                 CXXFLAGS="${DEFAULT_CXXFLAGS}" \
                 LDFLAGS="${DEFAULT_LDFLAGS}" \
                 --prefix="${PREFIX}" > "${LOGS_DIR}/config.log" 2>&1
-            make -j$(nproc) > "${LOGS_DIR}/make.log" 2>&1
+            make -j"$(nproc)" > "${LOGS_DIR}/make.log" 2>&1
             make install > "${LOGS_DIR}/install.log" 2>&1
         ); then
         ret=1
@@ -131,7 +134,7 @@ function build_babeltrace()
                 CXXFLAGS="${DEFAULT_CXXFLAGS}" \
                 LDFLAGS="${DEFAULT_LDFLAGS}" \
                 --prefix="${PREFIX}" > "${LOG_DIR}/config.log" 2>&1
-            make -j$(nproc) > "${LOG_DIR}/make.log" 2>&1
+            make -j"$(nproc)" > "${LOG_DIR}/make.log" 2>&1
             make install > "${LOG_DIR}/install.log" 2>&1
         ); then
         ret=1
@@ -152,8 +155,8 @@ function build_modules()
     tag="$(git -C "${MODULES_SRC_DIR}" describe)" || true
     if [[ "${commit}" == "$(git -C "${MODULES_SRC_DIR}" rev-parse HEAD)" ]] || [[ "${commit}" == "${tag}" ]] ; then
         echo "lttng-modules already on commit '${commit}'" >&2
-        make -C "${MODULES_SRC_DIR}" modules_install INSTALL_MOD_PATH=$PREFIX/usr > install.log 2>&1
-        depmod --all --base-dir=$PREFIX/usr > depmod.log 2>&1
+        make -C "${MODULES_SRC_DIR}" modules_install INSTALL_MOD_PATH="$PREFIX/usr" > install.log 2>&1
+        depmod --all --base-dir="$PREFIX/usr" > depmod.log 2>&1
         return $ret
     fi
 
@@ -163,9 +166,9 @@ function build_modules()
             cd "${MODULES_SRC_DIR}"
             git clean -dxf >/dev/null
             git checkout "${commit}"
-            make -j$(nproc) > "${LOG_DIR}/make.log" 2>&1
-            make modules_install INSTALL_MOD_PATH=$PREFIX/usr > "${LOG_DIR}/install.log" 2>&1
-            depmod --all --base-dir=$PREFIX/usr > "${LOG_DIR}/depmod.log" 2>&1
+            make -j"$(nproc)" > "${LOG_DIR}/make.log" 2>&1
+            make modules_install INSTALL_MOD_PATH="$PREFIX/usr" > "${LOG_DIR}/install.log" 2>&1
+            depmod --all --base-dir="$PREFIX/usr" > "${LOG_DIR}/depmod.log" 2>&1
     ); then
         # It's okay if lttng-modules fails
         ret=0
@@ -205,7 +208,7 @@ function build_ust()
                 CXXFLAGS="${DEFAULT_CXXFLAGS}" \
                 LDFLAGS="${DEFAULT_LDFLAGS}" \
                 --prefix="${PREFIX}" > "${LOG_DIR}/config.log" 2>&1
-            make -j$(nproc) > "${LOG_DIR}/make.log" 2>&1
+            make -j"$(nproc)" > "${LOG_DIR}/make.log" 2>&1
             make install > "${LOG_DIR}/install.log" 2>&1
         ); then
         ret=1
@@ -248,7 +251,7 @@ function build_tools()
                 CXXFLAGS="${DEFAULT_CXXFLAGS}" \
                 LDFLAGS="${DEFAULT_LDFLAGS}" \
                 --prefix="${PREFIX}" > "${LOG_DIR}/config.log" 2>&1
-            make -j$(nproc) > "${LOG_DIR}/make.log" 2>&1
+            make -j"$(nproc)" > "${LOG_DIR}/make.log" 2>&1
             make install > "${LOG_DIR}/install.log" 2>&1
         ); then
         ret=1
@@ -270,7 +273,7 @@ function build_ust_benchmarks()
                 EXTRA_CPPFLAGS="${DEFAULT_CPPFLAGS}" \
                 EXTRA_LDFLAGS="${DEFAULT_LDFLAGS}" \
                 LTTNG_MODULES_DIR="${MODULES_SRC_DIR}"
-    make -C "${UST_BENCHMARKS_SRC_DIR}" -j$(nproc) \
+    make -C "${UST_BENCHMARKS_SRC_DIR}" -j"$(nproc)" \
                 EXTRA_CFLAGS="${DEFAULT_CFLAGS}" \
                 EXTRA_CPPFLAGS="${DEFAULT_CPPFLAGS}" \
                 EXTRA_LDFLAGS="${DEFAULT_LDFLAGS}" \

lava/kvm/generate_kvm_virsh.sh
@@ -20,7 +20,7 @@ export LIBVIRT_DEFAULT_URI="qemu:///system"
 tmp=$(mktemp)
 name=$1
 virsh vol-create-as --pool default --name "${name}.raw" --capacity 2G --format raw
-data_disk_path="$(virsh vol-path ${name}.raw --pool default)"
+data_disk_path="$(virsh vol-path "${name}.raw" --pool default)"
 sudo mkfs.ext4 "$data_disk_path"
 virt-install --print-xml \
     --name "$name" \

pipelines/images/distrobuild.sh
@@ -140,7 +140,7 @@ for IMAGE_DIR in "${IMAGE_DIRS[@]}" ; do
 done
 
 if [[ "${IMAGE_FILE}" == "" ]] ; then
-    fail 1 "Unable to find image file for '${OS}' in ${IMAGE_DIRS[@]}"
+    fail 1 "Unable to find image file for '${OS}' in ${IMAGE_DIRS[*]}"
 fi
 
 if grep -q -E 'XX[A-Za-z0-9_]+XX' "${IMAGE_FILE}" ; then
@@ -182,7 +182,7 @@ DISTROBUILDER_ARGS+=(
 )
 
 # Run the build
-${DISTROBUILDER_ARGS[@]}
+"${DISTROBUILDER_ARGS[@]}"
 
 # Import
 # As 'distrobuilder --import-into-incus=alias' doesn't work since it only
@@ -219,7 +219,7 @@ fi
 if [[ "${TEST}" == "true" ]] ; then
     set +e
     INSTANCE_NAME=''
-    if INSTANCE_NAME="$(incus -q launch -e ${VM_ARG[@]} -p default -p "${INCUS_INSTANCE_PROFILE}" "${FINGERPRINT}")" ; then
+    if INSTANCE_NAME="$(incus -q launch -e "${VM_ARG[@]}" -p default -p "${INCUS_INSTANCE_PROFILE}" "${FINGERPRINT}")" ; then
         INSTANCE_NAME="$(echo "${INSTANCE_NAME}" | cut -d':' -f2 | tr -d ' ')"
         CLEANUP+=(
             "incus stop -f ${INSTANCE_NAME}"

commit c1bf57ba8ba15f66e3193e6440ae876f49b8a653

jjb: Fix coverity builds
When running coverity jobs, the builds fail with the following error:

    + cov-build --dir /home/jenkins/workspace/****_master_coverity/cov-int '' make -j8 V=1
    Coverity Build Capture (64-bit) version 2024.12.1 on Linux 6.12.63+deb13-amd64 x86_64
    Internal version numbers: 3c60fc625b p-2024.12-push-36
    [ERROR] Build command not found. ''

This problem was introduced in commit
60c207c1de072a9432b7269f7a02b39ed002cbd7 ("Fix shellcheck warnings").

By changing the default value of `COVERITY_SCAN_BUILD_OPTIONS` from a
string to an array, the quoted variable expansion works as expected.

Change-Id: Ia9b2caace17cc3234af45f81aeedf10779ff8535
Signed-off-by: Kienan Stewart <kstewart@efficios.com>

scripts/common/coverity.sh
@@ -15,7 +15,7 @@ WORKSPACE=${WORKSPACE:-}
 #COVERITY_SCAN_TOKEN=""
 COVERITY_SCAN_DESCRIPTION="Automated CI build"
 COVERITY_SCAN_NOTIFICATION_EMAIL="ci-notification@lists.lttng.org"
-COVERITY_SCAN_BUILD_OPTIONS=""
+COVERITY_SCAN_BUILD_OPTIONS=()
 
 DEPS_INC="$WORKSPACE/deps/build/include"
 DEPS_LIB="$WORKSPACE/deps/build/lib"