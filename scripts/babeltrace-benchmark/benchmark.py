db3e69214429f8da1f9cf7f9b0a0488ba8f27d26
Kienan Stewart <kstewart@efficios.com>
2026-05-19 11:54:58 -0400

    scripts: Use python3 from env for benchmark scripts
    This makes testing and running in a venv easier since it doesn't
    depend on invoking `python3 script_name` directly, and subprocess
    calls from pytest will work naturally.
    
    Change-Id: I28771573518410462e80e588292dbba725a45274
    Signed-off-by: Kienan Stewart <kstewart@efficios.com>

--- scripts/babeltrace-benchmark/benchmark.py
+++ scripts/babeltrace-benchmark/benchmark.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # SPDX-FileCopyrightText: 2019 Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
--- scripts/lttng-ust-benchmarks/benchmark.py
+++ scripts/lttng-ust-benchmarks/benchmark.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # SPDX-FileCopyrightText: 2026 Kienan Stewart <kstewart@efficios.com>
 # SPDX-License-Identifier: GPL-2.0-only
 #
